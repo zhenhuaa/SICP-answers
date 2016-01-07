1192f426b32ff423ab4d8a4530af39e47ec7e51c
zhenhua.wang <wangzhenhuaa@gmail.com>
2016-01-07 22:23:03 +0800

    complete ex1-11

--- ex1-11.scm
+++ ex1-11.scm
@@ -2,7 +2,17 @@
 !#
 
 (define (f n)
-  (if (< n 3) n 
-    (+ (f (- n 1)) (* 2 (f (- n 2)) (* 3 (f (- n 3)))))))
+  (if (< n 3) n
+    (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
 
-(display (f 4))
+(define (f2 n)
+  (define (f-iter a b c n)
+    (if (= n 2) c
+      (f-iter b c (+ (* a 3) (* 2 b) c) (- n 1))))
+  (if (< n 3) n
+    (f-iter 0 1 2 n)))
+
+(display "By Recursive\n")
+(display (f 10))
+(display "By Iterate\n")
+(display (f2 10))
