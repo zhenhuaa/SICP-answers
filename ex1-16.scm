253605d0b1d79ec5f27a1a4899d0fcbc4d65c60f
zhenhua.wang <wangzhenhuaa@gmail.com>
2016-01-08 01:06:41 +0800

    add ex1-16.scm

---
+++ ex1-16.scm
@@ -0,0 +1,19 @@
+#! /usr/bin/env csi -s
+
+(define (fast-expt b n)
+  (define (square x) (* x x))
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (fast-expt-iter b n)
+  (define (fast-expt-iter-helper acc b n)
+    (cond ((= n 0) acc)
+          ((odd? n) (fast-expt-iter-helper (* acc b) b (- n 1)))
+          (else (fast-expt-iter-helper acc (* b b) (/ n 2)))))
+  (fast-expt-iter-helper 1 b n))
+
+(use test)
+(test (expt 3 4) (fast-expt 3 4))
+(test (expt 3 4) (fast-expt-iter 3 4))
+(test (expt 3 9) (fast-expt-iter 3 9))
