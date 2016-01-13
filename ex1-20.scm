142e5c349fa8d563ffdd9b8e3194745a479e976a
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-13 17:19:17 +0800

    add ex1-20.scm

---
+++ ex1-20.scm
@@ -0,0 +1,21 @@
+#! /usr/bin/env csi -s
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (square x) (* x x))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((= (remainder n test-divisor) 0) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(use test)
+(test 3 (smallest-divisor 15))
+(test 7 (smallest-divisor 77))
+
+(display (smallest-divisor 199))
+(newline)
+(display (smallest-divisor 1999))
+(newline)
+(display (smallest-divisor 19999))
