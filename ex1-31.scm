91f59616a42b596e1607c0ce74d2376474fbcf0d
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-16 13:15:18 +0800

    add ex1-31.scm

---
+++ ex1-31.scm
@@ -0,0 +1,33 @@
+#! /usr/bin/env csi -s
+
+(define (identity x) x)
+(define (inc n) (+ n 1))
+
+(define (product term a next b)
+  (if (> a b) 1
+    (* (term a) (product term (next a) next b))))
+
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(use test)
+(test 6 (factorial 3))
+(test 24 (factorial 4))
+
+(define (cal-pi n)
+  (define (term n)
+    (/ (* (- n 1) (+ n 1)) (* n n)))
+  (define (next n) (+ n 2))
+  (* 4 (product term 3 next n)))
+
+(display (cal-pi 1000))
+(newline)
+
+
+
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b) result (iter (next a) (* (term a) result))))
+  (iter a 1))
+(display (cal-pi 1000))
