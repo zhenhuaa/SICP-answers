c6226d329dee496e16efa2da533c1604e9460ffc
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-16 09:48:28 +0800

    add ex1-30.scm

---
+++ ex1-30.scm
@@ -0,0 +1,17 @@
+#! /usr/bin/env csi -s
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b) result (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+
+; test
+(define (identity x) x)
+(define (inc x) (+ x 1))
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(use test)
+(test 15 (sum-integers 1 5))
+(test 55 (sum-integers 1 10))
