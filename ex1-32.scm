db688e00d775c721e712415ede16135802f71f5c
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-16 13:26:43 +0800

    add ex1-32.scm

---
+++ ex1-32.scm
@@ -0,0 +1,33 @@
+#! /usr/bin/env csi -s
+
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (inc n) (+ n 1))
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+(define (factorial n)
+  (product identity 1 inc n))
+
+(use test)
+(test 15 (sum-integers 1 5))
+(test 24 (factorial 4))
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b) result (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+(use test)
+(test 15 (sum-integers 1 5))
+(test 24 (factorial 4))
