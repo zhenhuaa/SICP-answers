0807db9b692005f9c29c3be2a822736f3fac83d2
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-27 13:04:57 +0800

    add ex1-43.scm

---
+++ ex1-43.scm
@@ -0,0 +1,10 @@
+#! /usr/bin/env csi -s
+
+(define (square x) (* x x))
+(define (compose f g) (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (<= n 1) f (compose f (repeated f (- n 1)))))
+
+(use test)
+(test 625 ((repeated square 2) 5))
