6a1e8ffc65a8530349266f80735e74293667d432
zhenhuaa <wangzhenhuaa@gmail.com>
2016-01-29 10:40:57 +0800

    add ex2-6.scm

--- ch2/ex2-6.scm
+++ ch2/ex2-6.scm
@@ -17,3 +17,10 @@
 (lambda (f) (lambda (x) (f (f x))))
 (define two (lambda (f) (lambda (x) (f (f x)))))
 
+(define (add m n) (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
+;apply zero and one to add
+(lambda (f) (lambda (x) (((lambda (f) (lambda (x) (x))) f) (((lambda (f) (lambda (x) (f x)))f) x))))
+(lambda (f) (lambda (x) ((lambda (x) (x)) ((lambda (x) (f x)) x))))
+(lambda (f) (lambda (x) ((lambda (x) (x)) (f x))))
+(lambda (f) (lambda (x) (f x))) ; got one
