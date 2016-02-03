310fafb41f2b0f99b2c397bf9767acec2426cc78
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-03 11:04:24 +0800

    ex2-17.scm

---
+++ ch2/ex2-17.scm
@@ -0,0 +1,11 @@
+#! /usr/bin/env csi -s
+
+(define (last-pair items)
+  (if (null? (cdr items))
+    (list (car items))
+    (last-pair (cdr items))))
+
+(use test)
+
+(test (list 2) (last-pair (list 1 2)))
+(test (list 1) (last-pair (list 1)))
