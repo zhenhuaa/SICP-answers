e4965f7496ad3bd93341b19f965968032252914b
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-03 14:41:09 +0800

    add ex2-22.scm

---
+++ ch2/ex2-22.scm
@@ -0,0 +1,18 @@
+#! /usr/bin/env csi -q
+
+(define (square x) (* x x))
+
+(define (list-push items x)
+  (if (null? items) (list x)
+    (cons (car items) (list-push (cdr items) x))))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things) (list-push answer (square (car things))))))
+
+  (iter items '()))
+
+(use test)
+(test '(1 4 9 16) (square-list '(1 2 3 4)))
