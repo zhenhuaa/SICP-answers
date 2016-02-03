3e76d9e50d49b96084d01d15da2c0f7a00e114ae
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-03 15:55:38 +0800

    add ex2-27.scm

---
+++ ch2/ex2-27.scm
@@ -0,0 +1,11 @@
+#! /usr/bin/env csi -q
+
+(define (deep-reverse x)
+  (if (null? x) x
+    (append (deep-reverse (cdr x))
+            (list (if (pair? (car x)) (deep-reverse (car x)) (car x))))))
+
+(use test)
+(define x (list (list 1 2) (list 3 4)))
+(test '((3 4) (1 2)) (reverse x))
+(test '((4 3) (2 1)) (deep-reverse x))
