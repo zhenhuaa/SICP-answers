d4e9d7aa47ffa301db82f3df11309cff96ec8c1d
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-03 18:53:54 +0800

    ex2-30 rename a function square-list => square-tree

--- ch2/ex2-30.scm
+++ ch2/ex2-30.scm
@@ -1,20 +1,20 @@
 #! /usr/bin/env csi -q
 
 (define (square x) (* x x))
-(define (square-list items)
+(define (square-tree items)
   (cond ((null? items) items)
         ((number? (car items))
          (cons (square (car items))
-               (square-list (cdr items))))
-        (else (cons (square-list (car items))
-                    (square-list (cdr items))))))
+               (square-tree (cdr items))))
+        (else (cons (square-tree (car items))
+                    (square-tree (cdr items))))))
 
-(define (square-list items)
+(define (square-tree items)
   (map (lambda (x)
          (cond ((null? x) x)
                ((number? x) (square x))
-               (else (square-list x))))
+               (else (square-tree x))))
        items))
 
 (use test)
-(test '(1 (4 (9 16) 25) (36 49)) (square-list '(1 (2 (3 4) 5) (6 7))))
+(test '(1 (4 (9 16) 25) (36 49)) (square-tree '(1 (2 (3 4) 5) (6 7))))
