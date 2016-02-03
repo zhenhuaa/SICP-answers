d02b47e78b4ea66d0e74631639cb07238226b7f3
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-03 13:31:41 +0800

    add ex2-19.scm

---
+++ ch2/ex2-19.scm
@@ -0,0 +1,19 @@
+#! /usr/bin/env csi -s
+
+
+(define (cc amount coin-values)
+  (cond
+    ((= amount 0 ) 1)
+    ((or (< amount 0) (no-more? coin-values)) 0)
+    (else
+      (+ (cc amount (except-first-denomination coin-values))
+         (cc (- amount (first-denomination coin-values)) coin-values)))))
+
+(define us-coins (list 50 25 10 5 1))
+
+(define (no-more? coin-values) (null? coin-values))
+(define (except-first-denomination coin-values) (cdr coin-values))
+(define (first-denomination coin-values) (car coin-values))
+
+(use test)
+(test 292 (cc 100 us-coins))
