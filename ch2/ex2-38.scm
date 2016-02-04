cd504ecfc8d39bf356f5af066a937c6bb9ec0a87
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-04 19:32:50 +0800

    add ex2-38.scm

---
+++ ch2/ex2-38.scm
@@ -0,0 +1,19 @@
+#! /usr/bin/env csi -q
+
+(define (fold-left op initial seq)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest)) (cdr rest))))
+  (iter initial seq))
+
+(use test)
+
+(define nil '())
+(test 1.5 (fold-right / 1 '(1 2 3)))
+(test (/ 1 6) (fold-left / 1 (list 1 2 3)))
+(test '(1 (2 (3 ()))) (fold-right list nil '(1 2 3)))
+(test '(((() 1) 2) 3) (fold-left list nil '(1 2 3)))
+
+
+; (op a b) === (op b a)
