9eac278ff6ea6cf1d9deb597e9b6b65df8f14d35
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-04 20:43:44 +0800

    fix ex2-39.scm

--- ch2/ex2-39.scm
+++ ch2/ex2-39.scm
@@ -4,13 +4,19 @@
 (define (reverse seq)
   (fold-right (lambda (x y) (append y (list x))) nil seq))
 
+(define (fold-left op initial seq)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest)) (cdr rest))))
+  (iter initial seq))
+
 (use test)
 (test '(1 2) (reverse (reverse '(1 2))))
 (test '(1 2 3 4) (reverse (reverse '(1 2 3 4))))
 
 (define (reverse seq)
-  (define fold-left fold)
-  (fold-left (lambda (x y) (cons x y)) nil seq))
+  (fold-left (lambda (result first) (cons first result))nil seq))
 
 (use test)
 (test '(1 2) (reverse (reverse '(1 2))))
