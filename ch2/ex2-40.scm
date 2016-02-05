16445c6c5a8c6c9a2ae805370c5c75a126b0ab43
zhenhua.wang <wangzhenhuaa@gmail.com>
2016-02-05 22:24:27 +0800

    add ex2-40.scm

---
+++ ch2/ex2-40.scm
@@ -0,0 +1,21 @@
+#! /usr/bin/env csi -q
+
+(define nil '())
+(define (enumerate-interval beg end)
+  (if (> beg end) nil (cons beg (enumerate-interval (+ beg 1) end))))
+
+(define (unique-pairs n) (flat-map
+                           (lambda (i) (map (lambda (j) (list i j)) (enumerate-interval 1 (- i 1))))
+                           (enumerate-interval 1 n)))
+
+(define (flat-map proc seq) (fold-right append nil (map proc seq)))
+
+(use test)
+(test '(1 2 3) (enumerate-interval 1 3))
+(test '(2 3 4) (enumerate-interval 2 4))
+(test '((2 1) (3 1) (3 2)) (unique-pairs 3))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum (filter prime-sum? (unique-pairs n))))
+
+
