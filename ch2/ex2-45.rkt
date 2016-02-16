aa71f089f8bc304d28ff08768a3ac53d2fe5aab0
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-16 14:17:38 +0800

    add ex2-45.rkt

---
+++ ch2/ex2-45.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(define (split op1 op2 )
+  (define (split-helper painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller (split-helper painter (- n 1))))
+        (op1 painter (op2 smaller smaller)))))
+  (lambda (painter n) (split-helper painter n)))
