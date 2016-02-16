923d2fcaa6ddfe58fd2aaa1b56be820d09f26b84
zhenhuaa <wangzhenhuaa@gmail.com>
2016-02-16 18:03:28 +0800

    add ex2-47.scm

---
+++ ch2/ex2-47.scm
@@ -0,0 +1,36 @@
+#! /usr/bin/env csi -q
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (list-ref frame 0))
+
+(define (edge1-frame frame)
+  (list-ref frame 1))
+
+(define (edge2-frame frame)
+  (list-ref frame 2))
+
+(use test)
+(define f (make-frame 1 2 3))
+(test 1 (origin-frame f))
+(test 2 (edge1-frame f))
+(test 3 (edge2-frame f))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (cddr frame))
+
+(define f (make-frame 1 2 3))
+(test 1 (origin-frame f))
+(test 2 (edge1-frame f))
+(test 3 (edge2-frame f))
