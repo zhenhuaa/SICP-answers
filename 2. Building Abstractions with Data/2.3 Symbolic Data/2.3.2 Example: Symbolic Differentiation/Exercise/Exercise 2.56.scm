ec39ad00ef6dd9b60ad414b14ca7c91b3b851eff
zhenhua wang <wangzhenhuaa@gmail.com>
2020-02-22 06:10:57 +0800

    finish 2.56.scm

--- 2. Building Abstractions with Data/2.3 Symbolic Data/2.3.2 Example: Symbolic Differentiation/Exercise/Exercise 2.56.scm
+++ 2. Building Abstractions with Data/2.3 Symbolic Data/2.3.2 Example: Symbolic Differentiation/Exercise/Exercise 2.56.scm
@@ -7,4 +7,19 @@ procedures exponentiation?, base, exponent, and make-exponentiation.
 (You may use the symbol ** to denote exponentiation.) 
 Build in the rules that anything raised to the power 0 is 1 and 
 anything raised to the power 1 is the thing itself.
-#|
+|#
+
+
+(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+
+; The base is the second item of the sum list:
+(define (base s) (cadr s))
+
+; The exponent is the third item of the sum list:
+(define (exponent s) (caddr s))
+
+(define (make-exponentiation base exponent) 
+    [cond ((=number? exponent) 0)
+          ((=number> exponent 1), base)
+          (else (list '** base exponent))
+    ])
--- 2. Building Abstractions with Data/2.3 Symbolic Data/2.3.2 Example: Symbolic Differentiation/note.scm
+++ 2. Building Abstractions with Data/2.3 Symbolic Data/2.3.2 Example: Symbolic Differentiation/note.scm
@@ -69,7 +69,8 @@
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
   ((=number? m1 1) m2)
   ((=number? m2 1) m1)
-  ((and (number? m1) (number? m2)) (* m1 m2)) (else (list '* m1 m2))))
+  ((and (number? m1) (number? m2)) (* m1 m2)) 
+  (else (list '* m1 m2))))
 
 (puts (deriv '(+ x 3) 'x))
 (puts (deriv '(* x y) 'x))
