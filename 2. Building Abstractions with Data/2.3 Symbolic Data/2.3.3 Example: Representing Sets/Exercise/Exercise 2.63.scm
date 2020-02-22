f5f26e7ffdb4fed0b27ee5fb9cddb97799ed4b87
zhenhua wang <wangzhenhuaa@gmail.com>
2020-02-22 22:03:55 +0800

    add 2.63.scm

---
+++ 2. Building Abstractions with Data/2.3 Symbolic Data/2.3.3 Example: Representing Sets/Exercise/Exercise 2.63.scm
@@ -0,0 +1,45 @@
+#|
+
+Exercise 2.63: Each of the following two procedures converts a binary tree to a list.
+
+a. Do the two procedures produce the same result for
+every tree? If not, how do the results differ? What lists
+
+b. Do the two procedures have the same order of growth in the number of
+ steps required to convert a balanced tree with n elements to a list? 
+ If not, which one grows more slowly?
+
+|#
+
+(load "../note.scm")
+
+(define (tree->list-1 tree) 
+    (if (null? tree) '()
+    (append (tree->list-1 (left-branch tree))
+        (cons (entry tree) (tree->list-1
+        (right-branch tree)))))) 
+
+(define (tree->list-2 tree)
+    (define (copy-to-list tree result-list) 
+        (if (null? tree) result-list
+            (copy-to-list (left-branch tree)
+                (cons (entry tree) 
+                      (copy-to-list (right-branch tree) result-list)))))
+    (copy-to-list tree '()))
+
+(define fig2-16-1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ())))) 
+(define fig2-16-2 '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ()))))) 
+(define fig2-16-3 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ())))) 
+
+; test
+(import trace)
+(trace tree->list-1)
+; (trace tree->list-2)
+
+(define puts ())
+
+(display "Trace list-1 ")
+(tree->list-1 fig2-16-1)
+(display "Trace list-2 ")
+; (tree->list-2 fig2-16-1)
+
