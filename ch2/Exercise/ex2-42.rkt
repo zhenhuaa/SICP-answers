e590173e7038f5a1222e89fe01d9b7f64705c793
zhenhua <wangzhenhuaa@gmail.com>
2021-03-18 00:14:19 +0800

    feat: use rackunit test

--- ch2/Exercise/ex2-42.rkt
+++ ch2/Exercise/ex2-42.rkt
@@ -1,19 +1,20 @@
-#! /usr/bin/env csi -q
+#lang racket/base
+(require srfi/1 rackunit)
 
 (define nil '())
 (define (enumerate-interval beg end)
   (if (> beg end) nil (cons beg (enumerate-interval (+ beg 1) end))))
-(define (flatmap proc seq) (fold-right append nil (map proc seq)))
+(define (flatmap proc seq) (foldr append null (map proc seq)))
 
 (define (queens board-size)
   (define (queen-cols k)
     (if (= k 0)
       (list empty-board)
       (filter
-        (lambda (positions) (safe? k positions))
+        (λ (positions) (safe? k positions))
         (flatmap
-          (lambda (rest-of-queens)
-            (map (lambda (new-row)
+          (λ (rest-of-queens)
+            (map (λ (new-row)
                    (adjoin-position new-row k rest-of-queens))
                  (enumerate-interval 1 board-size)))
           (queen-cols (- k 1))))))
@@ -25,7 +26,10 @@
 
 
 (define (same-row? p1 p2) (= (car p1) (car p2)))
-(define (same-diagonal? p1 p2) (= 1 (abs (/ (- (car p1) (car p2)) (- (cdr p1) (cdr p2))))))
+(define (same-diagonal? p1 p2)
+  (= 1
+    (abs (/ (- (car p1) (car p2))
+            (- (cdr p1) (cdr p2))))))
 
 (define (position-check? p1 p2)
   (or (same-row? p1 p2)
@@ -34,12 +38,11 @@
 (define (safe? k positions)
   (let ((k-queen-position (car positions))
         (rest-queen-positions (cdr positions)))
-    (not (any (lambda (position)
+    (not (any (λ (position)
                 (position-check? position k-queen-position))
               rest-queen-positions))))
 
 
-(import test)
-(test 1 (length (queens 1)))
-(test 2 (length (queens 4)))
-(test 92 (length (queens 8)))
+(check-equal? (length (queens 1)) 1 "1 queens")
+(check-equal? (length (queens 4)) 2 "4 queens")
+(check-equal? (length (queens 8)) 92 "8 queues")
